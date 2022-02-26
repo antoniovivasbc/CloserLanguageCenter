db7bc8d590f84fb9762fbd36856c044fee1332bc
Antonio Vivas <antoniovivascavalcante@gmail.com>
2022-02-26 15:08:42 +0200

    first commit

---
+++ conexao/conexao (3).php
@@ -0,0 +1,8 @@
+<?php 
+    $host = "localhost";
+    $usuario = "closerla_antonio";
+    $senha = "12345";
+    $bd = "closerla_closerlanguage";
+
+    $mysqli = new mysqli($host, $usuario, $senha, $bd);
+?>
